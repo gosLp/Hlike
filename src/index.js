4e3b8f6f936eae799729dfef7d617a5db46082d0
Pratheek Shetty <68729469+gosLp@users.noreply.github.com>
2023-03-04 00:21:33 +0530

    cpu values fetched in js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 document.addEventListener("DOMContentLoaded", ()=>{
     let i =0;
-     setInterval(()=>{
-         i =i+1;
+     setInterval(async ()=>{
+        let response = await fetch('/api/cpus');
+        if (response.status != 200){
+            throw new Error(`HTTp error! status: ${response.status}`);
+        }
+        let json = await response.json();
+        // i =i+1;
         
-         document.body.textContent = `cycle ${i}`;
+         document.body.textContent = JSON.stringify(json, null, 2);
      }, 1000)
     
 });
